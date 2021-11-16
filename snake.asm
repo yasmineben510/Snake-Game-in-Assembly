408aca51b8c2af23f3468935949a8c1b578d50f2
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-11-16 22:10:56 +0100

    updated

--- snake.asm
+++ snake.asm
@@ -31,29 +31,7 @@ addi    sp, zero, LEDS
 main:
     ; TODO: Finish this procedure.
 	call clear_leds ; on sait pas oourquoi ca marche ni ce que l'on fait SOS SVP
-	addi a0, zero, 3
-	addi a1, zero, 0
-	call set_pixel
-	addi a0, zero, 7
-	addi a1, zero, 1
-	call set_pixel
-
-	addi a0, zero, 5
-	addi a1, zero, 3
-	call set_pixel
-	
-	addi a0, zero, 2
-	addi a1, zero, 4
-	call set_pixel
-
-	addi a0, zero, 10
-	addi a1, zero, 2
-	call set_pixel
-
-	addi a0, zero, 11
-	addi a1, zero, 3
-	call set_pixel
-	
+	call get_input
 	
 	
     ret
@@ -72,28 +50,26 @@ clear_leds:
 
 ; BEGIN: set_pixel
 set_pixel:
-
 	;compute the LEDS WORD adress
 	srli t0, a0, 2 ; shifts right by 2 (divides by 4)
 	slli t0, t0, 2 ; shifts left by 2 : Word adress In lEDS stored in t0
 
-	;compute the index of the word to set
+	;----compute the index of the word to set---
 	andi t1, a0, 3 ; acts as a modulo 4 {[x]mod4}
 	slli t1, t1, 3 ; shifts [x]mod4 left by 3 (multiplies by 8) {8*x}
 	add t1, t1, a1; {8[x]mod4 + y}
 
-	;computes the word to store
+	;-----computes the word to store----
 	addi t3, zero, 1 ; stores the value 1
 	sll t2,t3, t1 ; shifts the value 1 by t1 indexes
-	;loads word and make an or
+	;----loads word and make an or------
 	ldw t4, LEDS(t0)
 	or t2, t4, t2 ; previous value OR new value (bitwise)
 	
-	;store the new word 
+	;----store the new word----
 	stw t2, LEDS(t0)
 
 	ret
-
 ; END: set_pixel
 
 
@@ -131,7 +107,7 @@ get_input:
     
     bne  s0, zero, checkpoint    ; branches to checkpoint if buttons were pushed
     addi  v0, zero, 0            ; returns 0
-    jmpi  end                    ; jumps to end 
+    jmpi  endGET                 ; jumps to endGET 
 
 
 checkpoint:
@@ -141,14 +117,14 @@ checkpoint:
 
     beq  t2, zero, directions    ; branches to direction if checkpoint button hasn't been pressed
     addi  v0, zero, 5            ; returns 5
-    jmpi  end                    ; jumps to end 
+    jmpi  endGET                 ; jumps to endGET 
 
 
 directions:
     
     addi t0, t0, 1               ; increment current by one
-    sll t1, s0, t0               ; 
-    andi t1, t1, 1               ; gives the to'th bit of the edgecapture, with step above
+    srl t1, s0, t0               ; 
+    andi t1, t1, 1               ; gives the t0'th bit of the edgecapture, with step above
     beq t1, zero, directions     ; if the bit is not 1, it is not the right one, we loop
     add v0, zero, t0             ; return value
     jmpi  state
@@ -162,25 +138,23 @@ state:
 computeState:
 
     addi t3, t3, 1               ; increment current by one
-    sll t1, s0, t3               ; 
+    srl t1, s0, t3               ; 
     andi t1, t1, 1               ; gives the t3'th bit of the edgecapture, with step above
     beq t1, zero, state          ; if the bit is not 1, it is not the right one, we loop again
    
-    addi t4, v0, t3              ; add our pushed button with our status, if the addition is equal to 5 we do not modify the state of our snake
+    add t4, v0, t3              ; add our pushed button with our status, if the addition is equal to 5 we do not modify the state of our snake
     addi t5, zero, 5            
     bne  t5, t4, changeState
-    jmpi end
+    jmpi endGET
 
 
 changeState:
    
     
 
-end: 
+endGET: 
     stw zero, (BUTTONS + 4)(zero); sets edgecapture at 0
     ret
-    
-
 ; END: get_input
 
 
@@ -192,6 +166,109 @@ draw_array:
 
 ; BEGIN: move_snake
 move_snake:
+;-----------possible orientations values --------
+	addi t1, zero, 1 ; left
+	addi t2, zero, 2 ; up
+	addi t3, zero, 3 ; down
+	addi t4, zero, 4 ; right
+
+;-------value of snake's head -----------
+	ldw t5, HEAD_X(zero) ; position x of head
+	ldw t6, HEAD_Y(zero) ; position y of head
+
+	slli t0, t5, 3 ; multiplies x by 8
+	add t0, t0, t6 ; adds 8 => linear adress of head on GSA
+
+	ldw t0, GSA(t0) ; VALUE of the SNAKE HEAD IN THE GSA => Orientation vector head
+
+;----flow---
+	beq t0, t1, left
+	beq t0, t2, up
+	beq t0, t3, down
+	beq t0, t4, right
+
+;---updates the HEAD X AND HEAD Y COORDINATES ; NOTE Still needs to compute mod 12 for the new x and y
+left: ; updates the x coord to the left 
+	ldw t7, HEAD_X(zero) ; position x of head
+	addi t5, t7, -1
+	stw t5, HEAD_X(zero)
+	jmpi endHead
+up:  ; updates the y coord to the up 
+	ldw t7, HEAD_Y(zero) ; position y of head
+	addi t5, t7, -1
+	stw t5, HEAD_Y(zero)
+	jmpi endHead
+
+down:
+	 ; updates the y coord to the down 
+	ldw t7, HEAD_Y(zero) ; position y of head
+	addi t5, t7, 1
+	stw t5, HEAD_Y(zero)
+	jmpi endHead
+
+right:  ; updates the x coord to the right 
+	ldw t7, HEAD_X(zero) ; position x of head
+	addi t5, t7, 1
+	stw t5, HEAD_X(zero)
+	jmpi endHead
+
+endHead:
+	ldw t5, HEAD_X(zero) ; position x of head
+	ldw t6, HEAD_Y(zero) ; position y of head
+
+	slli t7, t5, 3 ; multiplies x by 8
+	add t7, t7, t6 ; adds 8 => linear adress of head on GSA
+
+	stw t0, GSA(t0) ;sets the orientation vector of the head (same as the last head)
+
+	;--------value of snake's tail ---------------
+	ldw t5, TAIL_X(zero) ; position x of tail
+	ldw t6, TAIL_Y(zero) ; position y of tail
+
+	slli t7, t5, 3 ; multiplies x by 8
+	add t0, t7, t6 ; adds 8 => linear adress of tail on GSA in t0
+
+	ldw t7, GSA(t0) ; VALUE of the SNAKE TAIL IN THE GSA => Orientation vector tail
+
+	;------compute new tail coord----
+
+	beq t7, t1, lefty
+	beq t7, t2, upy
+	beq t7, t3, downy
+	beq t7, t4, righty
+	jmpi endTail
+;---updates the TAIL X AND TAIL Y COORDINATES ; NOTE Still needs to compute mod 12 for the new x and y
+
+lefty: ; updates the x coord to the left 
+	;position x in t5
+	addi t5, t5, -1
+	stw t5, TAIL_X(zero)
+	jmpi endTail
+upy:  ; updates the y coord to the up 
+	 ; position y of tail in t6
+	addi t6, t6, -1
+	stw t6, TAIL_Y(zero)
+	jmpi endTail
+
+downy:
+	 ; updates the y coord to the down 
+	 ; position y of tail in t6
+	addi t6, t6, 1
+	stw t6, TAIL_Y(zero)
+	jmpi endTail
+
+righty:  ; updates the x coord to the right 
+	; position x of tail in t5
+	addi t5, t5, 1
+	stw t5, TAIL_X(zero)
+	jmpi endTail
+
+endTail:
+;-----Clear last tail position---
+	addi t7, zero, 0
+	stw t7, GSA(t0)
+	ret
+
 
 ; END: move_snake
 
